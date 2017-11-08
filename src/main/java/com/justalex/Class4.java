7b194d1fa3bf464942366c2bcd12ca48c95fb998
Oleksii_Cherevatyi <oleksii_cherevatyi@epam.com>
2017-11-08 15:07:14 +0200

    Add Class4 fil and 3 day of week constants

---
+++ src/main/java/com/justalex/Class4.java
@@ -0,0 +1,9 @@
+package com.justalex;
+
+public class Class4 {
+
+    public static final String MONDAY = "M";
+    public static final String TUESDAY = "T";
+    public static final String WEDNESDAY = "W";
+
+}
