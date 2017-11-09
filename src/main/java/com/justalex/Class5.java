b469e7ddf07f221922cc7b1de2835bfb0068ddf9
Oleksii_Cherevatyi <oleksii_cherevatyi@epam.com>
2017-11-09 14:56:31 +0200

    Add SUNDAY constant

--- src/main/java/com/justalex/Class5.java
+++ src/main/java/com/justalex/Class5.java
@@ -5,5 +5,6 @@ public class Class5 {
     public static final String THURSDAY = "T";
     public static final String FRIDAY = "F";
     public static final String SATURDAY = "S";
+    public static final String SUNDAY = "S";
 
 }
