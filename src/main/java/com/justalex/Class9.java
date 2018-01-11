ce39be124be9ea8ca64c3fd1cbcc4169974d92b8
Oleksii Cherevatyi <alex.cherevatiy@gmail.com>
2018-01-11 15:48:04 +0200

    Create new Class9

---
+++ src/main/java/com/justalex/Class9.java
@@ -0,0 +1,9 @@
+package com.justalex;
+
+public class Class9 {
+
+    public static final int NUMBER_1 = 1;
+    public static final int NUMBER_2 = 2;
+    public static final int NUMBER_3 = 3;
+
+}
